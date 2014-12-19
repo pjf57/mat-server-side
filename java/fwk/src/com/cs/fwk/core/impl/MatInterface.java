0c8639724f180c7a3572885c561477f70ade4d95
Peter Fall <peter.fall@gmail.com>
2014-12-19 18:43:34 +0100

    committed version v1.03.000

--- java/fwk/src/com/cs/fwk/core/impl/MatInterface.java
+++ java/fwk/src/com/cs/fwk/core/impl/MatInterface.java
@@ -25,7 +25,7 @@ import com.cs.fwk.core.util.DesignUtils;
 
 public class MatInterface implements MatApi {
 
-	private static final String MI_VERS = "1.02.002";
+	private static final String MI_VERS = "1.03.000";
 
 	private final static Logger logger = Logger.getLogger(MatInterface.class);
 	private static final String CALCULATOR_PKG = "com.cs.fwk.core.config.calculators";
